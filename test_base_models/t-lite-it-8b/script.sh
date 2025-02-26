996b383d48265a31e0dba663e6ca1c3b28002ac4
Zeldrizz <zeldrizzyt@gmail.com>
2025-02-26 17:20:18 +0300

    Add base model T-LITE-IT-1.0 8b sample reponses

---
+++ test_base_models/t-lite-it-8b/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=test_model
+#SBATCH --error=logs/test_model_lite.err
+#SBATCH --output=logs/test_model_lite.log
+#SBATCH --time=04:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=4
+
+module load Python/Anaconda_v03.2023
+module load CUDA/12.2
+source activate my_py_env1
+
+python main.py
