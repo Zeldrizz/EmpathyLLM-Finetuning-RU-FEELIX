c8194d0b0a4d99da530ae210d99251d4aafd8ee2
Zeldrizz <zeldrizzyt@gmail.com>
2025-03-13 10:59:39 +0300

    Add FT | llama 3.1 8b ep 5 | YvvonM_mental_health_translated-filtered

---
+++ llama-3.1-8b-finetunes/llama-3.1-8b-ep5-YvvonM_mental_health_translated-filtered/main.py
@@ -0,0 +1,122 @@
+import os
+import torch
+import sys
+
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer
+)
+from peft import (
+    LoraConfig,
+    get_peft_model
+)
+from datasets import load_dataset
+from trl import SFTTrainer, SFTConfig, setup_chat_format
+
+if not torch.cuda.is_available():
+    print("GPU недоступно")
+    sys.exit(1)
+
+base_model = "/home/mrtaktarov/FEELIX_bot/models/Llama-3.1-8B-Instruct-RU"
+new_model = "llama-3.1-8b-it-Emotional-Support-ChatBot-RU"
+
+train_dataset_path = "/home/mrtaktarov/FEELIX_bot/datasets/mental_health_v2_dataset/mental_health_dataset_ru_train_90_90.csv"
+valid_dataset_path = "/home/mrtaktarov/FEELIX_bot/datasets/mental_health_v2_dataset/mental_health_dataset_ru_valid_90_10.csv"
+
+instruction = """
+Ты — эмпатичный и доброжелательный собеседник мужского пола по имени FEELIX. 
+Твоя задача — поддерживать собеседника в трудные моменты, помогать разбираться в эмоциях и создавать атмосферу доверия.
+
+Правила общения:
+- Фокусируйся на чувствах собеседника. Слушай внимательно, задавай уточняющие вопросы, не говори о себе, если тебя не спрашивают.
+- Поддерживай диалог, но если человек говорит о самоповреждении, насилии или тяжёлом кризисе, настойчиво предложи обратиться к специалисту. Ты можешь только оказывать словесную поддержку, но не имеешь права давать медицинские советы, назначать медикаменты или рекомендовать терапию.
+- Не затрагивай конфликтные темы (политика, религия). Если собеседник начинает такой разговор, мягко переведи его на другую тему.
+- Не предполагаешь пол собеседника, пока он сам этого не уточнит.
+- Отвечай на русском языке, если не поступила просьба сменить его.
+- Используй лаконичные и структурированные ответы, удобные для чтения.
+- Общайся всегда на "ты", если иначе не попросил собеседник.
+
+Ты создаёшь тёплую, доверительную атмосферу и помогаешь человеку осознать свои переживания без давления и навязывания решений.
+"""
+
+model = AutoModelForCausalLM.from_pretrained(
+    base_model,
+    device_map="auto",
+    torch_dtype=torch.float16,
+    trust_remote_code=True
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model,
+    trust_remote_code=True
+)
+
+train_dataset = load_dataset("csv", data_files=train_dataset_path)["train"]
+test_dataset = load_dataset("csv", data_files=valid_dataset_path)["train"]
+
+def format_chat_template(row):
+    row_json = [
+        {"role": "system", "content": instruction},
+        {"role": "user", "content": row["request"]},
+        {"role": "assistant", "content": row["response"]},
+    ]
+    row["text"] = tokenizer.apply_chat_template(row_json, tokenize=False)
+    return row
+
+train_dataset = train_dataset.map(format_chat_template, num_proc=4)
+test_dataset = test_dataset.map(format_chat_template, num_proc=4)
+
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"]
+)
+
+tokenizer.chat_template = None
+model, tokenizer = setup_chat_format(model, tokenizer)
+
+model = get_peft_model(model, peft_config)
+
+sft_config = SFTConfig(
+    output_dir=new_model,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    gradient_accumulation_steps=2,
+    optim="adamw_hf",
+    num_train_epochs=5,
+    do_eval=True,
+    evaluation_strategy="steps",
+    eval_steps=1000,
+    logging_steps=1,
+    warmup_steps=10,
+    logging_strategy="steps",
+    learning_rate=2e-4,
+    
+    fp16=True,  
+    bf16=False, 
+    
+    group_by_length=True,
+    max_seq_length=1024,
+    dataset_text_field="text",
+    packing=False,
+    report_to=None,
+
+    save_strategy="steps",
+    save_steps=5000,
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+    peft_config=peft_config,
+    processing_class=tokenizer,
+    args=sft_config
+)
+
+trainer.train()
+
+trainer.save_model(new_model)
---
+++ llama-3.1-8b-finetunes/llama-3.1-8b-ep5-YvvonM_mental_health_translated-filtered/run_model.py
@@ -0,0 +1,88 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+import sys
+
+if not torch.cuda.is_available():
+    print("GPU недоступен")
+    sys.exit(1)
+
+base_model_id = "/home/mrtaktarov/FEELIX_bot/models/Llama-3.1-8B-Instruct-RU"
+adapter_path = "/home/mrtaktarov/FEELIX_bot/llama-3.1-8b/llama-3.1-8b-try13/llama-3.1-8b-it-Emotional-Support-ChatBot-RU/checkpoint-10000"
+
+tokenizer = AutoTokenizer.from_pretrained(base_model_id, trust_remote_code=True)
+special_tokens = {"additional_special_tokens": ["<special_token_1>", "<special_token_2>"]}
+tokenizer.add_special_tokens(special_tokens)
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_id,
+    trust_remote_code=True,
+)
+base_model.resize_token_embeddings(len(tokenizer))
+
+model = PeftModel.from_pretrained(base_model, adapter_path)
+model.eval()
+model = model.to("cuda")
+
+SYS_PROMPT = """Ты — эмпатичный и доброжелательный собеседник мужского пола по имени FEELIX.
+Твой собеседник — студент, который сталкивается с учебными нагрузками, стрессом, поиском мотивации и балансом между учёбой и личной жизнью.
+Твоя задача — поддерживать его в трудные моменты, помогать разбираться в эмоциях и создавать атмосферу доверия.
+
+Правила общения:
+Фокусируйся на чувствах собеседника. Слушай внимательно, задавай уточняющие вопросы, помогай осознать переживания без давления.
+Поддерживай диалог, но не давай медицинских советов. Если студент говорит о самоповреждении, насилии или тяжёлом кризисе, мягко предложи обратиться к специалисту.
+Не затрагивай конфликтные темы (политика, религия). Если студент сам начинает такой разговор, переведи его в нейтральное русло.
+Не предполагаешь пол собеседника, пока он сам этого не уточнит.
+Отвечай на русском языке, если не поступила просьба сменить его.
+Структурируй ответы лаконично и понятно.
+Ты создаёшь тёплую, доверительную атмосферу и помогаешь студенту осознать свои эмоции, мотивировать себя и легче справляться с учёбой.
+"""
+
+questions = [
+    "Ты кто такой?",
+    "Мне все осточертело в этом мире. Хочу спрыгнуть с крыши, покончить со страданиями и перестать видеть этот гнилой мир",
+    "У меня страх перед экзаменом. Помоги мне.",
+    "Я постоянно откладываю дела на потом. Как перестать это делать?",
+    "У меня проблемы с общением со своими одногруппниками. Что посоветуешь?",
+    "Я чувствую себя выгоревшим от учебы. Что делать?",
+    "Как справляться с тревогой перед экзаменами и контрольными работами в университете?",
+    "Я катался на стуле в библиотеке и случайно упал с него. Все кто был в библиотеке увидели как я упал и посмеялись, я чувствую себя ужасно",
+]
+
+max_tokens_list = [1024]
+
+with open("responses_best-step.txt", "w", encoding="utf-8") as f:
+    f.write(f"Системный промпт: {SYS_PROMPT}\n\n")
+
+    for user_prompt in questions:
+        f.write(f"Вопрос: {user_prompt}\n\n")
+
+        for max_tokens in max_tokens_list:
+            messages = [
+                {"role": "system", "content": SYS_PROMPT},
+                {"role": "user", "content": user_prompt},
+            ]
+
+            prompt = tokenizer.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+
+            inputs = tokenizer(
+                prompt, return_tensors="pt", padding=True, truncation=True
+            ).to("cuda")
+
+            with torch.no_grad():
+                outputs = model.generate(
+                    **inputs,
+                    max_new_tokens=max_tokens,
+                    num_return_sequences=1,
+                    temperature=0.7,
+                    top_p=0.9,
+                )
+
+            response = tokenizer.decode(outputs[0], skip_special_tokens=True).split("assistant")[1]
+
+            f.write(f"Max new tokens: {max_tokens}\n")
+            f.write(f"Ответ модели: {response.strip()}\n\n")
+
+        f.write("\n\n")
